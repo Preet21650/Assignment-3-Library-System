40b1305f6fead659ab648f3a943b1af7c18db112
Preetmohan Manchanda <preetmohansm@outlook.com>
2021-01-14 14:54:45 +0700

    Add project files.

---
+++ Assignment 3 Library System/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_3_Library_System
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private bool borrowed;
+
+        public static int NumberOfBooks = 0;
+            
+        public Book(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.borrowed = false;
+
+            NumberOfBooks = NumberOfBooks + 1;
+
+        }
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public string GetAuthor()
+        {
+            return author;
+        }
+        public int GetPages()
+        {
+            return pages;
+        }
+        public bool isBorrowed()
+        {
+            return borrowed;
+        }
+
+        public void BorrowBook()
+        {
+            if (borrowed == false)
+            {
+                borrowed = true;
+
+            }
+            else
+            {
+                Console.WriteLine(title + "is not available");
+            }
+                    
+        }
+        public void ReturnBook()
+        {
+            if(borrowed == true)
+            {
+                borrowed = false;
+            }
+            else
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+}
---
+++ Assignment 3 Library System/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment_3_Library_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book harryPotter = new Book("Harry Potter", "JK Rowling", 399);
+            Console.WriteLine(Book.NumberOfBooks);
+
+            Book theBible = new Book("The Bible", "Jesus Christ", 999);
+            Console.WriteLine(Book.NumberOfBooks);
+
+            Book nineteeneightyfour = new Book("1984", "George Orwell", 599);
+            Console.WriteLine(Book.NumberOfBooks);
+
+        }
+    }
+}
